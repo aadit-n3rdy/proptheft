82f7417c479afe6697a2c8ace614b08e210060f8
ShreyasJenner <shreyasjenner4@gmail.com>
2023-12-14 09:52:05 +0530

    added sign up

--- backend/middleware/userauth.js
+++ backend/middleware/userauth.js
@@ -3,7 +3,7 @@ const pool = require('../pool/db');
 
 /* queries */
 loginText = 'SELECT u_passhash FROM db_user WHERE u_email = $1';
-signupText = 'UPDATE db_user SET u_passhash = $1 WHERE u_email = $2';
+signupText = 'INSERT INTO db_user VALUES ($1, $2, $3, $4, $5, $6)';
 
 const login = async (req, res) => {
   const response = await pool.query(loginText, [req.body.mail]);
@@ -20,17 +20,18 @@ const login = async (req, res) => {
 }
 
 const signUp = async (req, res) => {
-  console.log("inside")
-  console.log(req.query)
   const hash = await pass.genHash(req.body.pass)
-  const response = await pool.query(signupText, [hash,req.body.mail]);
-  console.log(response)
-  if(response.rowCount == 0) {
-    res.status(404)
-    res.send({msg: "Wrong email"});
-  } else {
-    res.status(200)
-    res.send({msg: "Signed in"})
+
+  try {
+    const response = await pool.query(signupText, [req.body.mail, req.body.fname, req.body.lname, req.body.phno,"user", hash]);
+    console.log(response)
+    res.status(200);
+    res.send({msg: "Signed Up"});
+  } catch(e) {
+    res.status(401);
+    if(e.code=="23505") {
+      res.send({err: "email already exists"});
+    }
   }
 }
 
